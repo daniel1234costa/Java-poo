7078efb80fd111d3d0107105fe2e1a9174230c7e
Francisco Daniel Costa de Souza <ds9324621@gmail.com>
2025-09-09 19:42:08 -0300

    feat:

--- Questões/Exercicio.java
+++ Questões/Exercicio.java
@@ -1,5 +1,5 @@
 
-/*public class Exercicio {
+public class Exercicio {
     public static void main (String[] args) {
 
         String nome = "Daniel";
@@ -10,7 +10,7 @@
 
     }
     
-} */
+} 
  
 /*import java.util.Scanner;
 public class Exercicio {
@@ -87,3 +87,97 @@ public static int fatorial (int num) {
 }
     
 }*/
+/*public class Triangulo{
+   float l1; 
+   float l2;
+   float l3;
+   String descicao;
+   void inicializaTriangulo(float lado1, float lado2,float lado3, String desc){
+
+    l1 = lado1;
+    l2 = lado2;
+    l3 = lado3;
+    descicao = desc;
+   }
+   float calculaPerimetro(){
+
+    
+   }
+}*/
+
+/**/
+ /*import java.util.Scanner;
+public class fibonacci {
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+        int n;
+        System.out.println("Informe um número para saber a série de fibonacci até sua posição correspondente: ");
+        n = leitor.nextInt();
+        for(int i = 1; i <= n; i++){
+        int resultado = calFibonacci(i);
+        System.out.printf(" %d  ", resultado);
+        }
+
+        leitor.close();
+        
+
+    }
+    public static int calFibonacci( int num){
+        if(num == 1){
+            return  0;
+        }
+        else if (num == 2 ){
+            return 1;
+        }
+        return calFibonacci( num - 1) + calFibonacci(num - 2);
+
+    }
+}
+    */
+    
+
+/*import java.util.Scanner;
+
+public class questao_22Polinomio {
+    public static void main(String[] args) {
+        int grau, valorDeX;
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Informe o grau do polinômio: ");
+        grau = leitor.nextInt();
+
+        System.out.print("Informe o valor de x: ");
+        valorDeX = leitor.nextInt();
+        int resultado = calcularPolinomio(grau, valorDeX, leitor);
+        System.out.println("Resultado do polinômio: " + resultado);
+
+        leitor.close();
+    }
+
+    /**
+     * A forma geral de um polinômio é:
+     *
+     * P(x) = a₀xⁿ + a₁xⁿ⁻¹ + a₂xⁿ⁻² + ⋯ + aₙ₋₁x + aₙ
+     *
+     * x é a variável do polinômio (pode ser um valor específico, como 2, 3, etc.).
+     *
+     * a₀, a₁, ..., aₙ são os coeficientes (valores que multiplicam as potências de
+     * x).
+     *
+     * n é o grau do polinômio, ou seja, a maior potência de x.
+     */
+/* 
+    public static int calcularPolinomio(int grauAtual, int x, Scanner leitor) {
+        System.out.print("Informe o coeficiente de grau " + grauAtual + ": ");
+        int coeficiente = leitor.nextInt();
+
+        if (grauAtual == 0) {
+            return coeficiente;
+        }
+
+        return x * calcularPolinomio(grauAtual - 1, x, leitor) + coeficiente;
+    }
+}
+*/
+
+ 
